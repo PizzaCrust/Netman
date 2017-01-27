51582fcd91377e0a62bfba5316bf91e4072d5171
PizzaCrust <pizzacrustcoder@gmail.com>
2017-01-27 23:15:08 +0700

    Added new PacketHandler and PacketListener - PacketHandler is similar to Forge's EventFactory where it handles when it retrieves the packet and sends it throughout the Netman pipeline. - PacketListener is used to listen for incoming Netman packets.

---
+++ src/main/java/online/pizzacrust/netman/api/PacketHandler.java
@@ -0,0 +1,41 @@
+package online.pizzacrust.netman.api;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Handles retrieving packets.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+public class PacketHandler {
+
+    private static final List<PacketListener> LISTENER_LIST = new ArrayList<>();
+
+    public static void registerPacketListener(PacketListener listener) {
+        LISTENER_LIST.add(listener);
+    }
+
+    public static void handleRetrievePacket(ByteArrayInputStream byteArrayInputStream) {
+        Optional<Class<?>> packetClass = Optional.empty();
+        DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
+        for (PacketFormat packetFormat : PacketFormatProvider.getRegisteredPacketFormats()) {
+            Class<?> identifiedClass = packetFormat.identifyPacketClass(dataInputStream);
+            if (identifiedClass != null) {
+                packetClass = Optional.of(identifiedClass);
+            }
+        }
+        ClassSerializer classSerializer = new ClassSerializer();
+        packetClass.ifPresent((clazz) -> {
+            Object packet = classSerializer.deserialize(dataInputStream, clazz);
+            if (packet != null) {
+                LISTENER_LIST.forEach((listener) -> listener.onRetrieve(packet));
+            }
+        });
+    }
+
+}
---
+++ src/main/java/online/pizzacrust/netman/api/PacketListener.java
@@ -0,0 +1,13 @@
+package online.pizzacrust.netman.api;
+
+/**
+ * Represents a packet listener to Netman packets.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+public interface PacketListener {
+
+    void onRetrieve(Object packet);
+
+}
