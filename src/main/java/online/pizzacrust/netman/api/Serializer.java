169f608ad2e7534345450e85e04a5a250e0d0437
PizzaCrust <pizzacrustcoder@gmail.com>
2017-01-27 21:43:35 +0700

    Target the specified target - Added @Target to target only the field element type. - Added @RetentionPolicy to keep the annotation during compile.

--- src/main/java/online/pizzacrust/netman/api/Serializer.java
+++ src/main/java/online/pizzacrust/netman/api/Serializer.java
@@ -1,11 +1,18 @@
 package online.pizzacrust.netman.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Defines the annotated element as a node that uses a custom serializer.
  *
  * @since 1.0-SNAPSHOT
  * @author PizzaCrust
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Serializer {
 
     /**
